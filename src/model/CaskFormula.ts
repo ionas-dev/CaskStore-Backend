6e311379020d2b20995e5773b02586f7b7eaee36
Jonas <Joonaskaiser@gmail.com>
2022-03-26 16:00:33 +0100

    Add methods to return all casks from brew api

--- src/model/CaskFormula.ts
+++ src/model/CaskFormula.ts
@@ -27,12 +27,59 @@ export class CaskFormula {
         cask.name = json.name[0];
         cask.allNames = json.name;
         cask.desc = json.desc;
+        cask.url = json.url;
+        cask.homepage = json.homepage;
         cask.install30 = json.analytics.install["30d"][cask.title!];
         cask.install90 = json.analytics.install["90d"][cask.title!];
         cask.install365 = json.analytics.install["365d"][cask.title!];
+
+        return cask;
+    }
+
+     /**
+     * Returns the cask formula created from the given json object without analytics.
+     * 
+     * @param json - The json object
+     * @returns The cask formula
+     */
+      static fromJsonWithoutAnalytics(json: any): CaskFormula {
+        let cask = new CaskFormula(json.token);
+        cask.name = json.name[0];
+        cask.allNames = json.name;
+        cask.desc = json.desc;
         cask.url = json.url;
         cask.homepage = json.homepage;
 
         return cask;
     }
+
+    /**
+     * Returns the cask formula created from the given json object wiht only analytics.
+     * 
+     * @param json - The json object
+     * @returns The cask formula
+     */
+     static fromJsonJustAnalytics(json: any, days: Days): CaskFormula {
+        let cask = new CaskFormula(json.cask);
+        let count = +(json.count as string).replace(",", "");
+        switch (days) {
+            case Days.THIRTY_DAYS: 
+                cask.install30 = count;
+                break;
+            case Days.NINETY_DAYS:
+                cask.install90 = count;
+                break;
+            case Days.YEAR:
+                cask.install365 = count;
+                break;
+        }
+        
+        return cask;
+    }
+}
+
+export enum Days {
+    THIRTY_DAYS = "30d",
+    NINETY_DAYS = "90d",
+    YEAR = "365d"
 }
--- src/repository/CaskFormulaRepository.ts
+++ src/repository/CaskFormulaRepository.ts
@@ -1,4 +1,4 @@
-import { CaskFormula } from 'src/model/CaskFormula';
+import { CaskFormula, Days } from '../model/CaskFormula';
 const fetch = require('node-fetch');
 
 /**
@@ -8,13 +8,13 @@ export class CaskFormulaRepository {
     static instance = new CaskFormulaRepository();
 
     /**
-     * Retruns the cask formula with the given name by the brew api. 
-     * If the cask formula is not found, null is returned.
+     * Returns the Cask Formula with the given name by the brew api. 
+     * If the Cask Formula is not found, null is returned.
      * 
-     * @param name - The name of the cask formula
-     * @returns The cask formula or null if not found
+     * @param name - The name of the Cask Formula
+     * @returns The Cask Formula or null if not found
      */
-     async getCaskFormula(name: string): Promise<CaskFormula | null> {
+    async getCaskFormula(name: string): Promise<CaskFormula | null> {
         const url = `https://formulae.brew.sh/api/cask/${name}.json`;
         const res: Response = await fetch(url);
 
@@ -27,12 +27,77 @@ export class CaskFormulaRepository {
         const obj = await res.json();
 
         try {
-            const cask = CaskFormula.fromJson(obj);
+            const caskFormula = CaskFormula.fromJson(obj);
             console.log(`Successfull fetch of '${name}'`)
-            return cask;
+            return caskFormula;
         } catch (e) {
             console.log(e);
             return null;
         }
+    }
+
+    /**
+     * Returns all Cask Formulas by the brew api. 
+     * If the Cask Formulas are not found, null is returned.
+     * 
+     * @returns The Cask Formulas or null if not found
+     */
+    async getAllCaskFormulas(): Promise<CaskFormula[] | null> {
+        const url = "https://formulae.brew.sh/api/cask.json";
+        const res: Response = await fetch(url);
+
+        // TODO: Handle every case specifically
+        if (!res.ok) {
+            console.log("Error: Cask not found");
+            return null;
+        }
+
+        const objs = await res.json();
+
+        const caskFormulas: CaskFormula[] = objs.map((obj: any) => {
+            try {
+                const caskFormula = CaskFormula.fromJsonWithoutAnalytics(obj);
+                return caskFormula;
+            } catch (e) {
+                console.log(e);
+                return null;
+            }
+        });
+
+        return caskFormulas;
+    }
+
+    /**
+     * Returns lightweight Cask Formulas with only the title and a installation count of the given period.
+     * The Cask Formulas are fetched by the brew api. 
+     * If the Cask Formulas are not found, null is returned.
+     * 
+     * @param days - The period of time to fetch the Cask Formulas
+     * @returns The Cask Formulas or null if not found
+     */
+    async getAllCaskFormulasWithOnlyAnalytics(days: Days): Promise<CaskFormula[] | null> {
+        const url = `https://formulae.brew.sh/api/analytics/cask-install/homebrew-cask/${days}.json`;
+        const res: Response = await fetch(url);
+
+        // TODO: Handle every case specifically
+        if (!res.ok) {
+            console.log("Error: Cask not found");
+            return null;
+        }
+
+        const objs = await res.json();
+        const caskFormulas: CaskFormula[] = [];
+
+        for (const obj in objs.formulae) {
+            try {
+                const caskFormula = CaskFormula.fromJsonJustAnalytics(objs.formulae[obj][0], days);
+                caskFormulas.push(caskFormula);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+
+        return caskFormulas;
     }
 }
