97bcc151389fefdcacc3f9844b52f1d6222b740e
TimGla <tim-gladbach@t-online.de>
2022-03-31 23:04:56 +0200

    Implemented icon fetching from cask homepage

--- src/Main.ts
+++ src/Main.ts
@@ -2,10 +2,47 @@ const { createServer } = require('@graphql-yoga/node');
 import "reflect-metadata";
 import { schema as schema_ } from './api/schema';
 import { createContext } from './api/context';
+import { getCaskImageFromHomepage } from './external/extImage'
+import { PrismaClient } from "@prisma/client";
 
 async function main() {
-    const schema = await schema_();
-    const server = createServer({ schema, context: createContext });
-    await server.start();
+    //const schema = await schema_();
+    //const server = createServer({ schema, context: createContext });
+    //await server.start();
+
+    const prisma = new PrismaClient();
+    var caskCounter = 1;
+    const casks = await prisma.cask.findMany({
+        select: {
+            id: true,
+            title: true,
+            homepage: true
+        },
+        where: {
+           id: 2257 
+        }
+    }).then(casks => {
+        casks.forEach(cask => {
+            if (cask.homepage != null) {
+                getCaskImageFromHomepage(cask.homepage, cask.id).then(result => {
+                    if (caskCounter % 200 == 0) console.log("Cask " + caskCounter + " of " + casks.length);
+                    if (result.length > 0) {
+                        result.forEach((imageUrl,index) => {
+                            const result = prisma.caskImage.create({
+                                data: {
+                                    title: cask.title,
+                                    type: 'icon',
+                                    url: imageUrl,
+                                    caskId: cask.id
+                                }
+                            }).then(res => {if (caskCounter % 200 == 0) console.log(res)});
+                        });
+                    }
+                    caskCounter++;
+                });
+            }
+        });
+    });
+
 }
 main();
--- src/external/extImage.ts
+++ src/external/extImage.ts
@@ -1,6 +1,56 @@
 const wiki = require('wtf_wikipedia');
 wiki.extend(require('wtf-plugin-image'));
 wiki.extend(require('wtf-plugin-classify'));
+import { parseFavicon } from "parse-favicon";
+const fs = require("fs");
+
+
+
+export async function getCaskImageFromHomepage(pageUrl: string, id:number): Promise<string[]> {
+
+    if (/^https:\/\/github.com.+$/.test(pageUrl)) {
+        //console.log("Cask has no homepage");
+        return [];
+    }
+    const result: string[] = [];
+    var gotAppleIcon: number = 0;
+    try {
+        await parseFavicon(pageUrl, textFetcher, bufferFetcher).forEach(async icon => {
+            var iconURL = icon.url;
+            if (!/^(http|https):\/\/.+/.test(iconURL)) {
+                if (/.+apple-touch-icon.+/.test(iconURL)) gotAppleIcon++;
+                iconURL = buildURL(pageUrl, icon.url);
+            }
+            if (/.+apple-touch-icon.+/.test(iconURL) && gotAppleIcon > 1) return;
+            if (result.length >= 5) return;
+            result.push(iconURL);
+        });
+    } catch(error) {
+        fs.appendFileSync("./external/errorUrls.txt", pageUrl + '-' + id + '\n');
+    }
+
+    async function textFetcher(url: string) {
+        return await fetch(resolveUrl(url, pageUrl)).then(res => res.text());
+    }
+      
+    async function bufferFetcher(url: string) {
+        return await fetch(resolveUrl(url, pageUrl)).then(res => res.arrayBuffer());
+    }
+      
+    function resolveUrl(url: string , base: string) {
+        return new URL(url, base).href;
+    }
+
+    return result;
+}
+
+
+function buildURL(pageUrl: string, icon: string): string {
+    const realPageUrl = pageUrl.split('/').splice(0,3).join('/');
+    const realIcon = (icon.charAt(0) == '/') ? icon.substring(1,icon.length) : icon;
+    return realPageUrl + '/' + realIcon;
+}
+
 
 /**
  * Fetches and returns url of image for the given cask names. 
